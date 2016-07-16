9fb950055fa4687ee54ddd4016090fd2748da1d3
Stoma-Viktor <stomchik1996@gmail.com>
2016-07-16 13:30:36 +0300

    Add files via upload

---
+++ WebUI/Models/CinemasListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class CinemasListViewModel
+    {
+        public IEnumerable<Cinema> Cinemas { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
