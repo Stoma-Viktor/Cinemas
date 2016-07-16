ce8f1fd23b273177882649981704c5a4153e499e
Stoma-Viktor <stomchik1996@gmail.com>
2016-07-16 13:24:03 +0300

    Add files via upload

--- WebUI/Constollers/CinemasController.cs
+++ WebUI/Constollers/CinemasController.cs
@@ -1,38 +1,43 @@
 ﻿using Domain.Abstract;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class CinemasController : Controller
     {
          private ICinemaRepository repository;
+         public int pageSize = 6;
          public CinemasController(ICinemaRepository repo)
         {
             repository = repo;
         }
-         public ViewResult List()
+         public ViewResult List(string category, int page = 1)
          {
-             return View(repository.Cinemas);
-         }
+              CinemasListViewModel model = new CinemasListViewModel
+            {
+                Cinemas = repository.Cinemas
+                .Where(b => category == null || b.Category == category )
+                 .OrderBy(Cinema => Cinema.CinemaId)
+                 .Skip((page-1)*pageSize)
+                 .Take(pageSize),
+                 PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems =category == null ? 
+                    repository.Cinemas.Count(): 
+                    repository.Cinemas.Where(p => p.Category == category).Count()
+                },
+                CurrentCategory = category
+            }; 
+              return View(model);
+            }            
 	}
 }
 
-namespace WebUI.Controllers
-{
-    public class CinemasController : Controller
-    {
-        private ICinemaRepository repository;
-        public CinemasController(ICinemaRepository repo)
-        {
-            repository = repo;
-        }
-        public ViewResult List()
-        {
-            return View(repository.Cinemas);
-        }
-    }
-}
---
+++ WebUI/Constollers/NavController.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private ICinemaRepository repository;
+
+        public NavController(ICinemaRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+
+
+            IEnumerable<string> categories = repository.Cinemas
+                .Select(p => p.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+        
+	}
+}
