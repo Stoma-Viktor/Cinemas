2a1fe617dc07e0e409296a42b3cbf016c69a85ed
Stoma-Viktor <stomchik1996@gmail.com>
2016-07-16 13:23:24 +0300

    Add files via upload

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -14,10 +14,34 @@ namespace WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Cinemas", action = "List", id = UrlParameter.Optional }
+                null,
+               "",
+               new { controller = "Cinemas", action = "List", category = (string)null, page = 1 }
+           );
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Cinemas", action = "List", category = (string)null},
+                 constraints: new { page = @"\d+" }
             );
+
+            routes.MapRoute(
+                null,
+               "{category}",
+               new { controller = "Cinemas", action = "List", page = 1 }               
+           );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Cinemas", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{controller}/{action}"
+            );
+            
         }
     }
 }
