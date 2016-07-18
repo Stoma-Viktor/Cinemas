f8cdf3f5dd5e3f683155f88cd539c55d953796cf
Stoma-Viktor <stomchik1996@gmail.com>
2016-07-18 14:25:43 +0300

    Add files via upload

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Cinema cinema, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.Cinema.CinemaId == cinema.CinemaId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Cinema = cinema,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Cinema cinema)
+        {
+            lineCollection.RemoveAll(l => l.Cinema.CinemaId == cinema.CinemaId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Cinema.Price * e.Quantity);
+
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+    public class CartLine
+    {
+        public Cinema Cinema { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Domain/Entities/Cinema.cs
+++ Domain/Entities/Cinema.cs
@@ -1,23 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Domain.Entities
 {
    public class Cinema
     {
+       [HiddenInput(DisplayValue = false)]
+       [Display(Name = "Id")]
        public int CinemaId { get; set; }
+       [Required(ErrorMessage = "Пожалуйста, введите название фильма")]
+       [Display(Name = "Название")]
        public string CinemaName { get; set; }
-        public string Description { get; set; }
+
+       [DataType(DataType.MultilineText)]
+       [Required(ErrorMessage = "Пожалуйста, введите описание фильма")]
+       [Display(Name = "Описание")]
+       public string Description { get; set; }
+       [Display(Name = "Жанр")]
+       [Required(ErrorMessage = "Пожалуйста, введите жанр фильма")]
         public string Category { get; set; }
+       [Display(Name = "Цена(руб)")]
+       [Required]
+       [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите корректную цену фильма")]
         public decimal Price { get; set; }
+       [Display(Name = "Год")]
+       [Required]
+       [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите корректный год выпуска фильма")]
         public int Year { get; set; }
+       [Required(ErrorMessage = "Пожалуйста, введите страну выпуска фильма")]
+       [Display(Name = "Страна")]
         public string Country { get; set; }
+       [Required(ErrorMessage = "Пожалуйста, введите режисёра фильма")]
+       [Display(Name = "Режисёр")]
         public string Directors { get; set; }
+       [Required(ErrorMessage = "Пожалуйста, введите актёров фильма")]
+       [Display(Name = "Актёры")]
         public string Artors { get; set; }
+       [Required]
+       [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите корректную продолжительность фильма")]
+       [Display(Name = "Время")]
         public int time { get; set; }
+
+       [Required(ErrorMessage = "Пожалуйста, введите путь к изображению")]
+       [Display(Name = "Постер(пока путь к файлу)")]
+       public string Image { get; set; }
+
+       public byte[] ImageData { get; set; }
+       public string ImageMimeType { get; set; }
 
     }
 }
