04b71e067c7d59a3ed25659b6dead11dc27d39a7
Stoma-Viktor <stomchik1996@gmail.com>
2016-07-18 14:27:18 +0300

    Add files via upload

--- WebUI/Infrasrtucture/NinjectDependencyResolver.cs
+++ WebUI/Infrasrtucture/NinjectDependencyResolver.cs
@@ -7,6 +7,8 @@ using Ninject;
 using Domain.Abstract;
 using Domain.Entities;
 using Domain.Concrete;
+using WebUI.Infrastructure.Abstract;
+using WebUI.Infrastructure.Concrete;
 
 namespace WebUI.Infrastructure
 {
@@ -33,6 +35,16 @@ namespace WebUI.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<ICinemaRepository>().To<EFCinemaRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                    .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
+            kernel.Bind<IAuthProvider>().To<FormAuthProvider>();
         }
     }
 }
